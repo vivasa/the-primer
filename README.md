3136feb185ed3682d47392190a4c99ce5b484933
Harikishore Tadigotla <harikishore@vivasa.in>
2022-06-23 18:23:14 +0530

    tmp folder

--- README.md
+++ README.md
@@ -7,10 +7,14 @@ This project is developed to help beginners with a starting point for coding the
 ---
 
 ## Prerequisites
-- JDK 11 or higher
-- Latest version of Docker and docker-compose 
+- To make best use of this project, a working setup of Docker (and docker-compose) is essential
+- JDK 11 or higher (not mandatory if docker is present)
 
 ## Steps to install
+### With Docker and docker compose
+
+`docker-compose up -d`
+
 ### Without docker, but JDK 11 is installed
 - Checkout the project to your local machine using `Git`
 - To build the project, run: `./gradlew build`
--- src/test/groovy/the/primer/GemControllerSpec.groovy
+++ src/test/groovy/the/primer/GemControllerSpec.groovy
@@ -27,13 +27,13 @@ class GemControllerSpec extends Specification {
     given:
       def qrRequest = [data: 'My Qr Input', path: 'tmp/myinp.png']
     when:
-      HttpRequest request = HttpRequest.POST("/primer/api/v1.0/gems/qr",qrRequest)
+      HttpRequest request = HttpRequest.POST("/gems/qr",qrRequest)
       HttpResponse response = client.toBlocking().exchange(request, Argument.of(Map.class), Argument.of(JsonError))
     then:
       response.status().code == 200
       response.body()['result'] == 'SUCCESS'
     when:
-      request = HttpRequest.GET("/primer/api/v1.0/gems/qr?path=${qrRequest.path}")
+      request = HttpRequest.GET("/gems/qr?path=${qrRequest.path}")
       response = client.toBlocking().exchange(request, Argument.of(Map.class), Argument.of(JsonError))
     then:
       response.status().code == 200
